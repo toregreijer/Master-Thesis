f6fdea39fd5cbf5f4fb52da2ab7cf75c31527fbe
nelsen <nelsen@student.chalmers.se>
2015-07-13 13:26:36 +0200

    deleted old files

--- Foo.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Joakim'
-from Networking import Networker
-raspberry = "192.168.0.125"
-n1 = Networker(raspberry)
-n1.send('Testicles!')
--- _main.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Joakim'
-from NetworkManager import Networker
-import DatabaseManager
-import MbusManager
-
-
-nwm = Networker()
