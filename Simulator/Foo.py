b3408be8305ae5ac5e7e3ed08d87463233ad0f0b
nelsen <nelsen@student.chalmers.se>
2015-07-13 12:16:43 +0200

    files from netbook

---
+++ Simulator/Foo.py
@@ -0,0 +1,8 @@
+__author__ = 'Joakim'
+#from Networking import Networker
+#raspberry = "192.168.0.161"
+#n1 = Networker(raspberry)
+#n1.send('Testicles!')
+print(int('0x11', 16))
+print(hex(229))
+print(chr(229))
---
+++ Simulator/Simulator.py
@@ -0,0 +1,24 @@
+__author__ = 'Joakim'
+import MeterUnitGateway
+from Networking import Networker
+
+
+class Simulator(object):
+    def __init__(self):
+        meters = MeterUnitGateway()
+        meters.listen()
+        networker = Networker()
+        networker.receive()
+
+"""
+respond to client
+ack_response = b'\xE5'
+rsp_ud = (b'\x68\x1D\x1D\x68'           # START:LENGTH:LENGTH:START
+          b'\x08\x01\x72'               # CONTROL:ADDRESS:CONTROL_INFO
+          b'\x44\x55\x66\x77\x88\x99'   # ACTIVE_DATA
+          b'\x11\x02\x00\x00\x00\x00'   # MORE DATA
+          b'\x86\x00\x82\xFF\x80\xFF'   # MORE DATA
+          b'\x00\x00\x00\x00\x00\x00'   # MORE DATA
+          b'\xDA\x0F\xCC\x16')          # DATA:DATA:CHECKSUM:STOP
+
+"""
