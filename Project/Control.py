562b41f098a054825d3944bff872aabcc7d44746
nelsen <toregreijer@gmail.com>
2015-09-04 23:48:44 +0200

    cleaned up control

--- Project/Control.py
+++ Project/Control.py
@@ -5,33 +5,44 @@ import MBus
 
 remote_host = '192.168.0.196'
 
+
+def scan():
+    """ Ping all addresses and return a list of those that respond """
+    list_of_addresses = []
+    for x in range(255):
+            if ping(x):
+                list_of_addresses.append(x)
+                print('Discovered unit at address {}!'.format(x))
+    return list_of_addresses
+
+
+def request_data(address):
+    """ Send REQ_UD2 to (address), store the response in the database. """
+    tmp = nm.send(MBus.req_ud2(address))
+    print('Sent request to {0}, got {1} back!'.format(address, tmp))
+    print('Storing stuff in database...')
+    open_and_store(tmp)
+    print('Done!')
+
+
+def ping(address):
+    """ Ping address and return the result, True or False. """
+    return nm.send(MBus.snd_nke(address))
+
+
 if __name__ == '__main__':
     try:
         print('Welcome to the Control Unit, please wait a moment!\n')
-        # SET UP THE DATABASE FOR FUTURE USE
         print('Setting up database...')
         setup_db()
-        # POLLING FOR DATA
-        # OPEN A CONNECTION TO THE MBUS MASTER
         print('Connecting to MBus...')
         nm = NetworkManager()
         nm.open_remote_socket()
 
-        for x in range(1):
-            # SEND A PING TO MBUS ADDRESS X
-            tmp = None
-            tmp = nm.send(MBus.snd_nke(x))
-            # IF THERE IS A UNIT AT ADDRESS X, REQUEST ITS DATA
-            if tmp:
-                tmp = nm.send(MBus.req_ud2(x))
-                print('Sent stuff, got {} back!'.format(int(tmp)))
-                # STORE THE DATA RECEIVED AT THE CORRECT PART OF THE DATABASE
-                print('Storing stuff in database...')
-                open_and_store(tmp)
-            else:
-                print('No answer at {}!'.format(x))
+        targets = scan()
+        for t in targets:
+            request_data(t)
 
-        # CLOSE DOWN THE CONNECTION
         print('Closing the connection...')
         nm.close_remote_socket()
         print('Exiting, goodbye!')
@@ -42,24 +53,3 @@ if __name__ == '__main__':
     except TimeoutError:
         print('Could not connect! (Timeout) Exiting...')
         sys.exit(0)
-
-
-def initialize():
-    # TODO: Put the setting up code here.
-    pass
-
-
-def scan():
-    # TODO: Ping address 0 through 255, store addresses that responds.
-    pass
-
-
-def request_data(address):
-    # TODO: Send REQ_UD2 to (address), store the response in the database.
-    pass
-
-
-def ping(address):
-    # TODO: As scan, but for only one (address).
-    pass
-
