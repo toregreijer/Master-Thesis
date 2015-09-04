659a11e37c65e75717fb8025748df5379856261b
nelsen <toregreijer@gmail.com>
2015-09-04 15:23:52 +0200

    more testing

--- Project/Testing.py
+++ Project/Testing.py
@@ -1,6 +1,8 @@
 foo = bytes.fromhex(str(18))
 manual = b'\x10\x40\x16' + foo
 bar = ':'.join('{:02X}'.format(x) for x in manual)
+list_of_hexes = [hex(h) for h in list(manual)]
 print('str: {0!s}  -  repr: {0!r}'.format(foo))
 print('str: {0!s}  -  repr: {0!r}'.format(manual))
 print('str: {0!s}  -  repr: {0!r}'.format(bar))
+print('str: {0!s}  -  repr: {0!r}'.format(list_of_hexes))
